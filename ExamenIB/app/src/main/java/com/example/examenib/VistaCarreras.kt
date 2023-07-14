18b4d9d4061eb40dc68d9e6b4df40b9149e22fdd
marioVillamar <mjvc98@hotmail.es>
2023-07-14 17:42:06 -0500

    Finalizacion

--- ExamenIB/app/src/main/java/com/example/examenib/VistaCarreras.kt
+++ ExamenIB/app/src/main/java/com/example/examenib/VistaCarreras.kt
@@ -51,7 +51,7 @@ class VistaCarreras : AppCompatActivity() {
             }
         }
 
-        
+
         val listView = findViewById<ListView>(R.id.lv_carreras)
         adaptador = ArrayAdapter(
             this,
