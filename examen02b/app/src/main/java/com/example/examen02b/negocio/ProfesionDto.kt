8b7c63466fb742d2c34f0e9e8dafcf9cc2d3af6c
marioVillamar <mjvc98@hotmail.es>
2023-09-13 17:26:39 -0500

    Borrado de proyectos mal configurados

--- examen02b/app/src/main/java/com/example/examen02b/negocio/ProfesionDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.examen02b.negocio
-
-class ProfesionDto(
-    var nombre: String,
-    var descripcion: String,
-    var activa: Boolean,
-    var salarioPromedio: Double
-) {
-}
--- examen2bmario/app/src/main/java/com/example/examen_2b_mario/negocio/ProfesionDto.kt
+++
@@ -1,9 +0,0 @@
-package com.frankz.examen_2b_mario.negocio
-
-class ProfesionDto(
-    var nombre: String,
-    var descripcion: String,
-    var activa: Boolean,
-    var salarioPromedio: Double
-) {
-}
